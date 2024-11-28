ab605f788dd6fac142602f86a7feed2dde547de7
Boris Demin <demin.b.d@gmail.com>
2024-11-29 00:07:22 +0500

    Обновлена логика класса WorldFactory, предназначенного для создания сущностей на карте их добавления при определенных условиях

--- src/main/java/code/rtfmyoumust/simulation/map/WorldFactory.java
+++ src/main/java/code/rtfmyoumust/simulation/map/WorldFactory.java
@@ -1,5 +1,6 @@
 package code.rtfmyoumust.simulation.map;
 
+import code.rtfmyoumust.simulation.model.Entity;
 import code.rtfmyoumust.simulation.model.livingEntities.Herbivore;
 import code.rtfmyoumust.simulation.model.livingEntities.Predator;
 import code.rtfmyoumust.simulation.model.staticEntities.Grass;
@@ -9,22 +10,10 @@ import code.rtfmyoumust.simulation.model.staticEntities.Tree;
 import java.util.Random;
 
 public class WorldFactory {
-
-    // задача расположить сущности в указанном диапазоне по х и у мирке
-    // при этом расставлять нужно только на пустые клетки
-    // генерация рандомного количества каждой сущности
-    // генерация рандомной координаты для каждой сущности
-
-    // распределение такое: 40% клетов должны быть свободными - 60% клеток под сущности
-    // тоесть на полученных значениях Х и У должны быть посчитаны 40% и 60%
-    // деревья и камни 15/60 - деревья 8 и камни 7
-    // растения 15/60
-    // травоядные 15/60
-    // хищники 15/60
-
-    static final double DISTRIBUTION_COEFF_OF_CREATURE = 0.15;
+    private static final double DISTRIBUTION_COEFF_OF_CREATURE = 0.15;
     private Random random = new Random();
     private World world;
+    private int entityPercent;
 
     private Coordinates generateRandomEmptyCoordinates(int x, int y) {
         Coordinates coordinates = null;
@@ -43,8 +32,8 @@ public class WorldFactory {
 
 
     public World createWorld(int sizeByX, int sizeByY) {
-        world = new World(sizeByX, sizeByY);
-        int entityPercent = (int) (sizeByX * sizeByY * DISTRIBUTION_COEFF_OF_CREATURE);
+        this.world = new World(sizeByX, sizeByY);
+        this.entityPercent = (int) (sizeByX * sizeByY * DISTRIBUTION_COEFF_OF_CREATURE);
 
         for (int i = 0; i <= entityPercent; i++) {
             world.setEntity(generateRandomEmptyCoordinates(sizeByX, sizeByY), Herbivore.class);
@@ -58,5 +47,13 @@ public class WorldFactory {
         }
 
         return world;
+    }
+
+    public World addEntity(World world, Class<? extends Entity> entity) {
+        this.world = world;
+        for (int i = 0; i <= this.entityPercent; i++) {
+            world.setEntity(generateRandomEmptyCoordinates(world.getGRID_WIDTH(), world.getGRID_HEIGHT()), entity);
+        }
+        return this.world;
     }
 }
