06fe613f9538fd84c9591283fec77a12adecd700
Boris Demin <demin.b.d@gmail.com>
2024-11-30 00:53:46 +0500

    Добавлена конструкция switch. Для неживых сущностей таких как Tree, Rock, Grass - решил оставить вызов конструктора через рефлексию. Добавлены константы для задания свойств объектов на этапе их создания.

--- src/main/java/code/rtfmyoumust/simulation/model/EntityFactory.java
+++ src/main/java/code/rtfmyoumust/simulation/model/EntityFactory.java
@@ -1,11 +1,31 @@
 package code.rtfmyoumust.simulation.model;
 
 import code.rtfmyoumust.simulation.map.Coordinates;
+import code.rtfmyoumust.simulation.model.livingEntities.Creature;
+import code.rtfmyoumust.simulation.model.livingEntities.Herbivore;
+import code.rtfmyoumust.simulation.model.livingEntities.Predator;
+import code.rtfmyoumust.simulation.model.staticEntities.Grass;
 
 public class EntityFactory {
+
+    private final static int HP = 100;
+    private final static int CREATURE_SPEED = 1;
+    private final static int HERBIVORE_HEALTH_LOSS = 10;
+    private final static int PREDATOR_HEALTH_LOSS = 20;
+    private final static int PREDATOR_ATTACK_STRENGTH = 40;
+    private final static int HERBIVORE_HEALTH_RECOVERY_FROM_EAT = 40;
+
     public static Entity createEntity(Class<? extends Entity> entityType, Coordinates coordinates) {
         try {
-            return entityType.getDeclaredConstructor(Coordinates.class).newInstance(coordinates);
+            return switch (entityType.getSimpleName()) {
+                case "Herbivore" -> new Herbivore(
+                        coordinates, HP, CREATURE_SPEED, Grass.class, HERBIVORE_HEALTH_LOSS,
+                        HERBIVORE_HEALTH_RECOVERY_FROM_EAT);
+                case "Predator" -> new Predator(
+                        coordinates, HP, CREATURE_SPEED, Herbivore.class, PREDATOR_HEALTH_LOSS,
+                        PREDATOR_ATTACK_STRENGTH);
+                default ->  entityType.getDeclaredConstructor(Coordinates.class).newInstance(coordinates);
+            };
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
