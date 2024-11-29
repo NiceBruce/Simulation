a62de5a3f8b9579c2974e75fda50acde1d02827a
Boris Demin <demin.b.d@gmail.com>
2024-11-30 00:38:10 +0500

    Добавлены дополнительные общие поля для живых существ. Добавлены дополнительные аргументы в конструктор. Метод feelHungry() перестал быть абстрактным и теперь имеет общую для потомков реализацию. Добавлен метод getStatistics() - имеет общую реализацию для потомков. В метод makeMove добавлен дополнительный аргумент - сущность(Entity)

--- src/main/java/code/rtfmyoumust/simulation/model/livingEntities/Creature.java
+++ src/main/java/code/rtfmyoumust/simulation/model/livingEntities/Creature.java
@@ -7,29 +7,29 @@ import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Creature extends Entity {
-
     private static int creatureCount;
+    private int creatureNumber;
+    protected int speed;
+    protected int hp;
+    protected Class<?> targetType;
+    protected int hungerHealthLoss;
+    protected static final int CLOSE_TO_TARGET = 2;
+    protected static final int NO_HEALTH = 0;
 
-    private int speed;
-    private int hp;
-    private Class<?> targetType;
-    public LinkedList<Coordinates> pathToTarget = new LinkedList<>();
-
-
-    public Creature(Coordinates coordinates) {
+    public Creature(Coordinates coordinates, int hp, int speed, Class<?> targetType, int healthLoss) {
         super(coordinates);
-        this.creatureCount++;
-        this.hp = 100;
+        this.hp = hp;
+        this.speed = speed;
+        this.targetType = targetType;
+        this.hungerHealthLoss = healthLoss;
+        creatureCount++;
+        this.creatureNumber = creatureCount;
     }
 
-    public abstract void makeMove(List<Coordinates> path);
+    public abstract void makeMove(List<Coordinates> path, Entity entity);
 
     public Class<?> getTargetType() {
         return this.targetType;
-    }
-
-    public int getCreatureCount() {
-        return creatureCount;
     }
 
     public int getHp() {
@@ -40,7 +40,22 @@ public abstract class Creature extends Entity {
         this.hp = hp;
     }
 
-    public abstract void feelHungry();
+    public void feelHungry() {
+        this.setHp(this.getHp() - this.hungerHealthLoss);
+    }
 
-    public abstract void getStatistics();
+    public int getCreatureNumber() {
+        return creatureNumber;
+    }
+
+    public void getStatistics() {
+        System.out.println(String.format("[%s #%s, position: [%s, %s], HP: %s, Status: %s]",
+                this.getClass().getSimpleName(),
+                this.getCreatureNumber(),
+                this.getCoordinates().getX(),
+                this.getCoordinates().getY(),
+                this.getHp(),
+                this.getHp() <= 0 ? "Died" : "Alive and Hungry!"
+        ));
+    };
 }
