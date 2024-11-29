6f663327117887308fae1bd1d520eda4f2e74fc6
Boris Demin <demin.b.d@gmail.com>
2024-11-29 16:06:30 +0500

    рефакторинг класса WorldRender. Метод 'renderMap' разбит на более мелкие методы + исправлена проверка типов существ.

--- src/main/java/code/rtfmyoumust/simulation/map/WorldRender.java
+++ src/main/java/code/rtfmyoumust/simulation/map/WorldRender.java
@@ -4,15 +4,14 @@ import code.rtfmyoumust.simulation.model.Entity;
 import code.rtfmyoumust.simulation.model.livingEntities.Creature;
 
 public class WorldRender {
-
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_EARTH_COLOR = "\u001B[48;2;1;168;1m";
     private static final String ANSI_DAMAGE_HEALT_COLOR = "\u001B[48;2;255;255;0m";
     private static final String ANSI_LOW_HEALTH_COLOR = "\u001B[48;2;255;0;0m";
 
     public static final void renderMap(World world, int countSimulation) {
-//        System.out.print("[Simulation iteration# " + countSimulation + "] ");
-//        world.printCountEntity();
+        System.out.print("[Simulation iteration# " + countSimulation + "] ");
+        world.printCountEntity();
 
         for (int y = world.getGRID_HEIGHT(); y >= 0; y--) {
             String line = "";
@@ -21,25 +20,14 @@ public class WorldRender {
                 if (world.isPositionEmpty(coordinates)) {
                     line += ANSI_EARTH_COLOR + "\uD83D\uDFE9" +  " ";
                 } else {
-                    if (world.getEntity(new Coordinates(x, y)) instanceof Creature) {
-                        Creature creature = (Creature) world.getEntity(new Coordinates(x, y));
-                        if (creature.getHp() < 100 && creature.getHp() > 50) {
-                            line += ANSI_DAMAGE_HEALT_COLOR + selectUnicodeForEntity(world.getEntity(coordinates)) + " ";
-                        } else if (creature.getHp() <= 50 ) {
-                            line += ANSI_LOW_HEALTH_COLOR + selectUnicodeForEntity(world.getEntity(coordinates)) + " ";
-                        } else {
-                            line += ANSI_EARTH_COLOR + selectUnicodeForEntity(world.getEntity(coordinates)) + " ";
-                        }
-                    } else {
-                        line += ANSI_EARTH_COLOR + selectUnicodeForEntity(world.getEntity(coordinates)) + " ";
-                    }
+                    line += colorEntity(world.getEntity(coordinates));
                 }
             }
             line = line.stripTrailing();
             line += ANSI_RESET;
             System.out.println(line);
         }
-//        System.out.println("Press 'p' to Pause, Press 'r' to Resume, press 'q' to Quit");
+        System.out.println("Press 'p' to Pause, Press 'r' to Resume, press 'q' to Quit");
     }
 
     public static final String selectUnicodeForEntity(Entity entity) {
@@ -59,4 +47,28 @@ public class WorldRender {
 
         return "";
     }
+
+    public static String colorEntity(Entity entity) {
+        String line;
+        if (entity instanceof Creature creature) {
+            line = colorCreatureHealth(creature);
+        } else {
+            line = ANSI_EARTH_COLOR + selectUnicodeForEntity(entity) + " ";
+        }
+        return line;
+    }
+
+    public static String colorCreatureHealth(Creature creature) {
+        String line;
+        if (creature.getHp() < 100 && creature.getHp() > 50) {
+            line = ANSI_DAMAGE_HEALT_COLOR;
+        } else if (creature.getHp() <= 50 ) {
+            line = ANSI_LOW_HEALTH_COLOR;
+        } else {
+            line = ANSI_EARTH_COLOR;
+        }
+
+        return line + selectUnicodeForEntity(creature) + " ";
+    }
+
 }
