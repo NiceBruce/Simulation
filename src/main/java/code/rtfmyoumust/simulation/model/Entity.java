5710a9c2ffa9fd722860d132f766796977479e68
Boris Demin <demin.b.d@gmail.com>
2024-11-30 00:31:47 +0500

    Исправлен модификатор доступа поля Coordinates на приватный. Удален конструктор без аргументов.

--- src/main/java/code/rtfmyoumust/simulation/model/Entity.java
+++ src/main/java/code/rtfmyoumust/simulation/model/Entity.java
@@ -3,11 +3,8 @@ package code.rtfmyoumust.simulation.model;
 import code.rtfmyoumust.simulation.map.Coordinates;
 
 public abstract class Entity {
+    private Coordinates coordinates;
 
-    public Coordinates coordinates;
-
-    public Entity() {
-    }
     public Entity(Coordinates coordinates) {
         this.coordinates = coordinates;
     }
