c72a94155c9d583af64d716904d67d399284a821
Boris Demin <demin.b.d@gmail.com>
2024-11-30 00:27:12 +0500

    Исправлен нейминг методов, убраны проверки через рефлексию

--- src/main/java/code/rtfmyoumust/simulation/search/engine/BFS.java
+++ src/main/java/code/rtfmyoumust/simulation/search/engine/BFS.java
@@ -2,6 +2,7 @@ package code.rtfmyoumust.simulation.search.engine;
 
 import code.rtfmyoumust.simulation.map.Coordinates;
 import code.rtfmyoumust.simulation.map.World;
+import code.rtfmyoumust.simulation.model.Entity;
 import code.rtfmyoumust.simulation.model.livingEntities.Creature;
 import code.rtfmyoumust.simulation.model.livingEntities.Herbivore;
 import code.rtfmyoumust.simulation.model.livingEntities.Predator;
@@ -12,9 +13,11 @@ import code.rtfmyoumust.simulation.model.staticEntities.Tree;
 import java.util.*;
 
 public class BFS {
-    World world;
-
-    Creature currentCreature;
+    private World world;
+    private Creature currentCreature;
+    private LinkedList<Coordinates> searched;
+    private ArrayList<Coordinates> passed;
+    private HashMap<Coordinates, List<Coordinates>> parents;
 
     public List<Coordinates> getChild(Coordinates currentCoordinate) {
         List<Coordinates> child = new ArrayList<>();
@@ -39,58 +42,61 @@ public class BFS {
     }
 
     public boolean checkCoordinate(int x, int y) {
+        Coordinates coordinates = new Coordinates(x, y);
+        Entity entity = world.getEntity(coordinates);
         if ((x >= 0 && x <= world.getGRID_WIDTH()) && (y >= 0  && y <= world.getGRID_HEIGHT())) {
-            if (world.isPositionEmpty(new Coordinates(x, y))) {
+            if (world.isPositionEmpty(coordinates)) {
                 return true;
-            } else if (this.currentCreature.getClass() == Predator.class) {
-                return (world.getEntity(new Coordinates(x, y)).getClass() != Predator.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Tree.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Grass.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Rock.class);
-            } else if (this.currentCreature.getClass() == Herbivore.class) {
-                return (world.getEntity(new Coordinates(x, y)).getClass() != Predator.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Tree.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Herbivore.class)
-                        && (world.getEntity(new Coordinates(x, y)).getClass() != Rock.class);
+            } else if (this.currentCreature instanceof Predator) {
+                return !(entity instanceof Predator)
+                        && !(entity instanceof Tree)
+                        && !(entity instanceof Grass)
+                        && !(entity instanceof Rock);
+            } else if (this.currentCreature instanceof Herbivore) {
+                return !(entity instanceof Predator)
+                        && !(entity instanceof Tree)
+                        && !(entity instanceof Herbivore)
+                        && !(entity instanceof Rock);
             }
         }
         return false;
     }
 
     public boolean isTarget(Coordinates coordinates, Class<?> targetType) {
-        if (world.getEntity(coordinates) == null) return false;
-        return world.getEntity(coordinates).getClass() == targetType;
+        Entity entity = world.getEntity(coordinates);
+        if (entity == null) return false;
+        return entity.getClass() == targetType;
     }
 
-    public List<Coordinates> getSource(World world, Coordinates startCoordinates, Creature cr, Class<?> targetType) {
+    public List<Coordinates> findPathBFS(World world, Creature creature, Class<?> targetType) {
         this.world = world;
-        this.currentCreature = cr;
-        LinkedList<Coordinates> searched = new LinkedList<>();
-        ArrayList<Coordinates> passed = new ArrayList<>();
-        HashMap<Coordinates, List<Coordinates>> parents = new HashMap<>(); //создаем структуру для хранения узла - его чайлды
+        currentCreature = creature;
+        Coordinates startCoordinates = creature.getCoordinates();
+        searched = new LinkedList<>();
+        passed = new ArrayList<>();
+        parents = new HashMap<>(); //создаем структуру для хранения узла - его чайлды
+
         searched.add(startCoordinates); // добавляем текущие координаты узла в очередь проверки
         passed.add(startCoordinates); // добавляем стартовыую координату в посещенные
 
         while(!searched.isEmpty()) { // пока очередь не пуста
             Coordinates currentCoordinates = searched.poll(); // достаем первую координату из очереди
             if (isTarget(currentCoordinates, targetType)) { // Является ли она целью?
-                return new ArrayList<>(getPath(parents, startCoordinates, currentCoordinates)); // Возвращаем структуру узел -чайлд в функцию поиска обратного пути
+                return new ArrayList<>(getPath(startCoordinates, currentCoordinates));
             }
 
-            for (Coordinates childNode : getChild(currentCoordinates)) {
+            for (Coordinates childNode : getChild(currentCoordinates)) { // пробегаемся по возможным дочерним узлам
                 if (!passed.contains(childNode)) { // если текущей координаты НЕТ в посещенных
                     passed.add(childNode);
                     searched.add(childNode);
                     parents.computeIfAbsent(currentCoordinates, v -> new ArrayList<>()).add(childNode);
                 }
             }
-
         }
-
         return new ArrayList<>();
     }
 
-    public ArrayDeque<Coordinates> getPath(Map<Coordinates, List<Coordinates>> parents , Coordinates start, Coordinates target) {
+    public ArrayDeque<Coordinates> getPath(Coordinates start, Coordinates target) {
         ArrayDeque<Coordinates> path = new ArrayDeque<>();
         path.push(target);
         while(target != start) {
@@ -101,8 +107,6 @@ public class BFS {
                 }
             }
         }
-
         return path;
     }
-
 }
