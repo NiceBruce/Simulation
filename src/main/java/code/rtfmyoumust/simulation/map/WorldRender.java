60ab4d505b61f22609060505b7ff56135721ce93
Boris Demin <demin.b.d@gmail.com>
2024-12-04 00:09:27 +0500

    refactor(WorldRender.java):  switch преобразован к линейному,  добавлено исключение в default. Убран можификатор final у методов класса. В методе renderMap переменная line теперь не String, а StringBuilder.

--- src/main/java/code/rtfmyoumust/simulation/map/WorldRender.java
+++ src/main/java/code/rtfmyoumust/simulation/map/WorldRender.java
@@ -9,43 +9,38 @@ public class WorldRender {
     private static final String ANSI_DAMAGE_HEALT_COLOR = "\u001B[48;2;255;255;0m";
     private static final String ANSI_LOW_HEALTH_COLOR = "\u001B[48;2;255;0;0m";
 
-    public static final void renderMap(World world, int countSimulation) {
+    public static void renderMap(World world, int countSimulation) {
         System.out.print("[Simulation iteration# " + countSimulation + "] ");
         world.printCountEntity();
 
         for (int y = world.getY(); y >= 0; y--) {
-            String line = "";
+            StringBuilder line = new StringBuilder();
             for (int x = 0; x <= world.getX(); x++) {
                 Coordinates coordinates = new Coordinates(x, y);
                 if (world.isPositionEmpty(coordinates)) {
-                    line += ANSI_EARTH_COLOR + "\uD83D\uDFE9" +  " ";
+                    line.append(ANSI_EARTH_COLOR + "\uD83D\uDFE9" + " ");
                 } else {
-                    line += colorEntity(world.getEntity(coordinates));
+                    line.append(colorEntity(world.getEntity(coordinates)));
                 }
             }
-            line = line.stripTrailing();
-            line += ANSI_RESET;
+            line = new StringBuilder(line.toString().stripTrailing());
+            line.append(ANSI_RESET);
             System.out.println(line);
         }
         System.out.println("Press 'p' to Pause, Press 'r' to Resume, press 'q' to Quit");
     }
 
-    public static final String selectUnicodeForEntity(Entity entity) {
+    public static String selectUnicodeForEntity(Entity entity) {
 
-        switch (entity.getClass().getSimpleName()) {
-            case "Herbivore":
-                return "\uD83D\uDC30";
-            case "Predator":
-                return "\uD83D\uDC3A";
-            case "Grass":
-                return "\uD83C\uDF3F";
-            case "Rock":
-                return "\uD83E\uDEA8";
-            case "Tree":
-                return "\uD83C\uDF33";
-            default:
-                throw new IllegalArgumentException("Неизвестный тип объекта: " + entity.getClass().getSimpleName());
-        }
+        return switch (entity.getClass().getSimpleName()) {
+            case "Herbivore" -> "\uD83D\uDC30";
+            case "Predator" -> "\uD83D\uDC3A";
+            case "Grass" -> "\uD83C\uDF3F";
+            case "Rock" -> "\uD83E\uDEA8";
+            case "Tree" -> "\uD83C\uDF33";
+            default ->
+                    throw new IllegalArgumentException("Неизвестный тип объекта: " + entity.getClass().getSimpleName());
+        };
     }
 
     public static String colorEntity(Entity entity) {
