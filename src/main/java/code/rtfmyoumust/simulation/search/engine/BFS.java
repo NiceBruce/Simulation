296a44b4e9c40d01ea6de11326b5c700c090059a
Boris Demin <demin.b.d@gmail.com>
2024-12-08 23:53:20 +0500

    fix: BFS remove unusable fields and create target Field. Refactor getChild(), checkCoordinate() and findPathBFS()

--- src/main/java/code/rtfmyoumust/simulation/search/engine/BFS.java
+++ src/main/java/code/rtfmyoumust/simulation/search/engine/BFS.java
@@ -14,75 +14,58 @@ import java.util.*;
 
 public class BFS {
     private World world;
-    private Creature currentCreature;
     private LinkedList<Coordinates> searched;
-    private ArrayList<Coordinates> passed;
-    private HashMap<Coordinates, List<Coordinates>> parents;
+    private List<Coordinates> passed;
+    private Map<Coordinates, List<Coordinates>> parents;
+    private Class<?> target;
 
-    public List<Coordinates> getChild(Coordinates currentCoordinate) {
+    private List<Coordinates> getChild(Coordinates currentCoordinate) {
         List<Coordinates> child = new ArrayList<>();
+        int[][] shifts = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
 
-        if (checkCoordinate(currentCoordinate.getX() + 1, currentCoordinate.getY())) {
-            child.add(new Coordinates(currentCoordinate.getX() + 1, currentCoordinate.getY()));
-        }
+        for (int[] shift : shifts) {
+            int x = currentCoordinate.getX() + shift[0];
+            int y = currentCoordinate.getY() + shift[1];
 
-        if (checkCoordinate(currentCoordinate.getX(),currentCoordinate.getY() - 1)) {
-            child.add(new Coordinates(currentCoordinate.getX(),currentCoordinate.getY() - 1));
-        }
-
-        if (checkCoordinate(currentCoordinate.getX() - 1, currentCoordinate.getY())) {
-            child.add(new Coordinates( currentCoordinate.getX() - 1, currentCoordinate.getY()));
-        }
-
-        if (checkCoordinate(currentCoordinate.getX(), currentCoordinate.getY() + 1)) {
-            child.add(new Coordinates(currentCoordinate.getX(), currentCoordinate.getY() + 1));
+            if (checkCoordinate(x, y)) {
+                child.add(new Coordinates(x, y));
+            }
         }
 
         return child;
     }
 
-    public boolean checkCoordinate(int x, int y) {
+    private boolean checkCoordinate(int x, int y) {
         Coordinates coordinates = new Coordinates(x, y);
-        Entity entity = world.getEntity(coordinates);
-        if ((x >= 0 && x <= world.getGRID_WIDTH()) && (y >= 0  && y <= world.getGRID_HEIGHT())) {
+        if ((x >= 0 && x <= world.getX()) && (y >= 0  && y <= world.getY())) {
             if (world.isPositionEmpty(coordinates)) {
                 return true;
-            } else if (this.currentCreature instanceof Predator) {
-                return !(entity instanceof Predator)
-                        && !(entity instanceof Tree)
-                        && !(entity instanceof Grass)
-                        && !(entity instanceof Rock);
-            } else if (this.currentCreature instanceof Herbivore) {
-                return !(entity instanceof Predator)
-                        && !(entity instanceof Tree)
-                        && !(entity instanceof Herbivore)
-                        && !(entity instanceof Rock);
+            } else {
+                return world.getEntity(coordinates).getClass() == target;
             }
         }
         return false;
     }
 
-    public boolean isTarget(Coordinates coordinates, Class<?> targetType) {
+    private boolean isTarget(Coordinates coordinates) {
         Entity entity = world.getEntity(coordinates);
         if (entity == null) return false;
-        return entity.getClass() == targetType;
+        return entity.getClass() == target;
     }
 
-    public List<Coordinates> findPathBFS(World world, Creature creature, Class<?> targetType) {
+    public List<Coordinates> findPathBFS(World world, Coordinates start, Class target) {
         this.world = world;
-        currentCreature = creature;
-        Coordinates startCoordinates = creature.getCoordinates();
+        this.target = target;
         searched = new LinkedList<>();
         passed = new ArrayList<>();
         parents = new HashMap<>(); //создаем структуру для хранения узла - его чайлды
-
-        searched.add(startCoordinates); // добавляем текущие координаты узла в очередь проверки
-        passed.add(startCoordinates); // добавляем стартовыую координату в посещенные
+        searched.add(start); // добавляем текущие координаты узла в очередь проверки
+        passed.add(start); // добавляем стартовыую координату в посещенные
 
         while(!searched.isEmpty()) { // пока очередь не пуста
             Coordinates currentCoordinates = searched.poll(); // достаем первую координату из очереди
-            if (isTarget(currentCoordinates, targetType)) { // Является ли она целью?
-                return new ArrayList<>(getPath(startCoordinates, currentCoordinates));
+            if (isTarget(currentCoordinates)) { // Является ли она целью?
+                return new ArrayList<>(getPath(start, currentCoordinates));
             }
 
             for (Coordinates childNode : getChild(currentCoordinates)) { // пробегаемся по возможным дочерним узлам
@@ -96,7 +79,7 @@ public class BFS {
         return new ArrayList<>();
     }
 
-    public ArrayDeque<Coordinates> getPath(Coordinates start, Coordinates target) {
+    private ArrayDeque<Coordinates> getPath(Coordinates start, Coordinates target) {
         ArrayDeque<Coordinates> path = new ArrayDeque<>();
         path.push(target);
         while(target != start) {
