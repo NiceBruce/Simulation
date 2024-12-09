f5a3156b42c519c296a540ef2dfb1be88f1f1485
Boris Demin <demin.b.d@gmail.com>
2024-12-09 23:48:33 +0500

    fix: World - remove unusable methods. Refactor getCreatures() -> getEntities(), removeCreature(). Deleted countEntity() and printCountEntity().

--- src/main/java/code/rtfmyoumust/simulation/map/World.java
+++ src/main/java/code/rtfmyoumust/simulation/map/World.java
@@ -1,16 +1,10 @@
 package code.rtfmyoumust.simulation.map;
 
 import code.rtfmyoumust.simulation.model.Entity;
-import code.rtfmyoumust.simulation.model.EntityFactory;
-import code.rtfmyoumust.simulation.model.alive.Creature;
-import code.rtfmyoumust.simulation.model.alive.Herbivore;
-import code.rtfmyoumust.simulation.model.alive.Predator;
-import code.rtfmyoumust.simulation.model.inanimate.Grass;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class World {
 
@@ -27,26 +21,20 @@ public class World {
         return entities.get(coordinates);
     }
 
-    public List<Creature> getCreatures() {
-        return this.entities.values().stream()
-                .filter(Creature.class::isInstance)
-                .map(Creature.class::cast)
-                .collect(Collectors.toList());
+    public List<Entity> getEntities(Coordinates coordinates) {
+        return new ArrayList<>(entities.values());
     }
 
-    public void setEntity(Coordinates coordinates, Class<? extends Entity> entityType) {
-        Entity entity = EntityFactory.createEntity(entityType, coordinates);
+    public void setEntity(Coordinates coordinates, Entity entity) {
         entity.setCoordinates(coordinates);
         entities.put(coordinates, entity);
     }
 
-    public void shiftEntity(Coordinates oldCoordinate, Coordinates newCoordinate, Entity entity) {
-        entities.remove(oldCoordinate);
-        entities.put(newCoordinate, entity);
-    }
-
     public void removeCreature(Coordinates coordinates) {
-        entities.remove(coordinates);
+        if (entities.containsKey(coordinates)) entities.remove(coordinates);
+        else {
+            throw new IllegalArgumentException("coordinate does not contain Entity");
+        }
     }
 
     public boolean isPositionEmpty(Coordinates coordinates) {
@@ -56,40 +44,7 @@ public class World {
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
-    }
-
-    public Map<Class<? extends Entity>, Integer> countEntity() {
-        int herbivoreCount = 0;
-        int predatorsCount = 0;
-        int grassCount = 0;
-
-        for (Map.Entry<Coordinates, Entity> entity : entities.entrySet()) {
-            switch (entity.getValue().getClass().getSimpleName()) {
-                case "Herbivore":
-                    herbivoreCount += 1;
-                    break;
-                case "Predator":
-                    predatorsCount += 1;
-                    break;
-                case "Grass":
-                    grassCount += 1;
-                    break;
-            }
-        }
-
-        return Map.of(Herbivore.class, herbivoreCount,
-                Predator.class, predatorsCount,
-                Grass.class, grassCount);
-    }
-
-    public void printCountEntity() {
-        Map<Class<? extends Entity>, Integer> entityCount = countEntity();
-        System.out.println(String.format("Herbivore: %s Predator: %s Grass: %s",
-                entityCount.get(Herbivore.class),
-                entityCount.get(Predator.class),
-                entityCount.get(Grass.class)));
     }
 }
