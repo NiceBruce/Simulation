54e97ddc6357222c8a4351c8a381c2315c776ae5
Boris Demin <demin.b.d@gmail.com>
2024-12-10 23:48:38 +0500

    fix: EntityFactory - remove using reflection API.

--- src/main/java/code/rtfmyoumust/simulation/model/EntityFactory.java
+++ src/main/java/code/rtfmyoumust/simulation/model/EntityFactory.java
@@ -4,6 +4,8 @@ import code.rtfmyoumust.simulation.map.Coordinates;
 import code.rtfmyoumust.simulation.model.alive.Herbivore;
 import code.rtfmyoumust.simulation.model.alive.Predator;
 import code.rtfmyoumust.simulation.model.inanimate.Grass;
+import code.rtfmyoumust.simulation.model.inanimate.Rock;
+import code.rtfmyoumust.simulation.model.inanimate.Tree;
 
 public class EntityFactory {
 
@@ -15,18 +17,17 @@ public class EntityFactory {
     private final static int HERBIVORE_HEALTH_RECOVERY_FROM_EAT = 40;
 
     public static Entity createEntity(Class<? extends Entity> entityType, Coordinates coordinates) {
-        try {
-            return switch (entityType.getSimpleName()) {
-                case "Herbivore" -> new Herbivore(
-                        coordinates, HP, CREATURE_SPEED, Grass.class, HERBIVORE_HEALTH_LOSS,
-                        HERBIVORE_HEALTH_RECOVERY_FROM_EAT);
-                case "Predator" -> new Predator(
-                        coordinates, HP, CREATURE_SPEED, Herbivore.class, PREDATOR_HEALTH_LOSS,
-                        PREDATOR_ATTACK_STRENGTH);
-                default ->  entityType.getDeclaredConstructor(Coordinates.class).newInstance(coordinates);
-            };
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return switch (entityType.getSimpleName()) {
+            case "Herbivore" -> new Herbivore(
+                    coordinates, HP, CREATURE_SPEED, Grass.class, HERBIVORE_HEALTH_LOSS,
+                    HERBIVORE_HEALTH_RECOVERY_FROM_EAT);
+            case "Predator" -> new Predator(
+                    coordinates, HP, CREATURE_SPEED, Herbivore.class, PREDATOR_HEALTH_LOSS,
+                    PREDATOR_ATTACK_STRENGTH);
+            case "Grass" -> new Grass(coordinates);
+            case "Rock" -> new Rock(coordinates);
+            case "Tree" -> new Tree(coordinates);
+            default -> throw new IllegalArgumentException("Unsupported entity type: " + entityType.getSimpleName());
+        };
     }
 }
